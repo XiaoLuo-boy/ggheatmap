0a9fc67d7cbc5b359f80f8e84d221cd927e16303
XiaoLuo-boy <65945397+XiaoLuo-boy@users.noreply.github.com>
2021-05-22 11:04:07 +0800

    Add files via upload

--- R/ggheatmap.R
+++ R/ggheatmap.R
@@ -7,14 +7,14 @@
 #' @param scale character,the way of scale data("none", "row" or "column")
 #' @param cluster_rows whether rows should be clustered(TRUE of FALSE)
 #' @param cluster_cols whether column should be clustered(TRUE of FALSE)
-#' @param dist_method character,the method parameter of dist function
-#' @param hclust_method character,the method parameter of hclust function
+#' @param dist_method character,the method parameter of dist function. see \link{dist}
+#' @param hclust_method character,the method parameter of hclust function, see \link{hclust}
 #' @param text_angle_rows numeric,the angle of y-axis label
 #' @param text_angle_cols numeric,the angle of x-axis label
 #' @param text_color_rows character,the color of y-axis label
 #' @param text_color_cols character,the color of x-axis label
-#' @param text_face_rows character,the face of y-axis label
-#' @param text_face_cols character,the face of x-axis label
+#' @param text_face_rows character,the face of y-axis label, see \link{element_text}
+#' @param text_face_cols character,the face of x-axis label, see \link{element_text}
 #' @param text_just_rows a vector(hjust,vjust) for the hjust/vjust of y-axis label
 #' @param text_just_cols a vector(hjust,vjust) for the hjust/vjust of x-axis label
 #' @param text_show_rows a character you want to show for y-axis
@@ -41,6 +41,102 @@
 #' @import tidyr
 #' @import grDevices
 #' @import stats
+#' @examples
+#' #Create data
+#' library(ggheatmap)
+#' set.seed(123)
+#' df <- matrix(runif(600,0,10),ncol = 12)
+#' colnames(df) <- paste("sample",1:12,sep = "")
+#' rownames(df) <- sapply(1:50, function(x)paste(sample(LETTERS,3,replace = FALSE),collapse = ""))
+#' head(df)
+#'
+#' #example 1:The usage of cluster_rows/cluster_cols
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE)
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = FALSE)
+#'
+#' #example 2:Scale data
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,scale = "column")
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,scale = "row")
+#'
+#' #example 3:Set legend title
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,legendName = "score")
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,legendName = NULL)
+#'
+#' #example 4:Adjust the coordinate label
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,text_angle_rows = 330,text_just_rows = c(1,0))
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,text_angle_cols = 45,text_just_cols = c(1,1))
+#'
+#' #example 5:Custom label color
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,text_color_cols = "red")
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,
+#'             text_color_cols = c(rep("black",6),"red",rep("black",5)))
+#'
+#' #example 6:Custom label face
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,text_face_rows = "bold")
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,
+#'           text_color_rows = c("red",rep("black",49)),
+#'           text_face_rows = c("bold",rep("italic",49)))
+#'
+#' #example 7:Adjust the color and height of the cluster tree
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,show_cluster_cols = FALSE)
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,tree_height_rows = 0.05,
+#'            tree_height_cols = 0.05)
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,cluster_num = c(5,4))
+#' ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,cluster_num = c(5,4),
+#'           tree_color_rows = c("#3B4992FF","#EE0000FF","#008B45FF",
+#'           "#631879FF","#008280FF"),
+#'           tree_color_cols = c("#0073C2FF", "#EFC000FF" ,"#868686FF", "#CD534CFF"))
+#'
+#' #example 8:Comprehensive usage1
+#' text_rows <- sample(rownames(df),3)
+#' ggheatmap(df,scale = "row",cluster_rows  = TRUE,cluster_cols = TRUE,cluster_num = c(2,3),
+#'           text_show_rows = text_rows,
+#'           text_face_rows = "bold",
+#'           text_color_rows = "red",
+#'           text_angle_cols = 45,
+#'           text_just_cols = c(1,1),
+#'           tree_color_rows = c( "#3B4992FF","#EE0000FF"),
+#'           tree_color_cols = c("#1F77B4FF","#FF7F0EFF","#2CA02CFF"))
+#'
+#' #sample 9:Annotation
+#' row_metaData <- data.frame(exprtype=sample(c("Up","Down"),50,replace = TRUE),
+#'                            genetype=sample(c("Metabolism","Immune","None"),50,replace = TRUE))
+#' rownames(row_metaData) <- rownames(df)
+#' col_metaData <- data.frame(tissue=sample(c("Normal","Tumor"),12,replace = TRUE),
+#'                            risklevel=sample(c("High","Low"),12,replace = TRUE))
+#' rownames(col_metaData) <- colnames(df)
+#' exprcol <- c("#EE0000FF","#008B45FF" )
+#' names(exprcol) <- c("Up","Down")
+#' genecol <- c("#EE7E30","#5D9AD3","#D0DFE6FF")
+#' names(genecol) <- c("Metabolism","Immune","None")
+#' tissuecol <- c("#98D352","#FF7F0E")
+#' names(tissuecol) <- c("Normal","Tumor")
+#' riskcol <- c("#EEA236FF","#46B8DAFF")
+#' names(riskcol) <- c("High","Low")
+#' col <- list(exprtype=exprcol,genetype=genecol,tissue=tissuecol,risklevel=riskcol)
+#'
+#' ggheatmap<- ggheatmap(df,cluster_rows = TRUE,cluster_cols = TRUE,scale = "row",
+#'                       cluster_num = c(5,3),
+#'                       tree_color_rows = c("#3B4992FF","#EE0000FF","#008B45FF",
+#'                       "#631879FF","#008280FF"),
+#'                       tree_color_cols = c("#1F77B4FF","#FF7F0EFF","#2CA02CFF"),
+#'                       annotation_rows = row_metaData,
+#'                       annotation_cols = col_metaData,
+#'                       annotation_color = col
+#' )
+#'
+#' #sample 10:Comprehensive usage2
+#' library(aplot)
+#' dat <- data.frame(marker=sample(c(1,NA),50,replace = TRUE),
+#'                   gene=rownames(df),
+#'                   shape=sample(c("T","F"),50,replace = TRUE))
+#' p <- ggplot(dat,aes(x=1,y=gene,size=marker,color=shape,shape=shape))+
+#'   geom_point()+theme_classic()+
+#'   scale_color_manual(values = c("#D2691E","#1E87D2"))+
+#'   theme(line = element_blank(),axis.text = element_blank(),axis.title = element_blank())+
+#'   guides(size = FALSE)
+#'
+#' ggheatmap%>%insert_right(p,width = 0.1)
 #' @export
 NULL
 
@@ -49,8 +145,8 @@ ggheatmap <- function(data,
                        color=colorRampPalette(c( "#0073c2","white","#efc000"))(100),
                        legendName="Express",
                        scale="none",
-                       cluster_rows = F,
-                       cluster_cols = F,
+                       cluster_rows = FALSE,
+                       cluster_cols = FALSE,
                        dist_method="euclidean",
                        hclust_method="complete",
                        text_angle_rows=0,
@@ -61,14 +157,16 @@ ggheatmap <- function(data,
                        text_face_cols=NULL,
                        text_just_rows=NULL,
                        text_just_cols=NULL,
+                       text_position_rows="right",
+                       text_position_cols="bottom",
                        text_show_rows=waiver(),
                        text_show_cols=waiver(),
                        annotation_cols=NULL,
                        annotation_rows=NULL,
                        annotation_color,
                        annotation_width=0.03,
-                       show_cluster_cols=T,
-                       show_cluster_rows=T,
+                       show_cluster_cols=TRUE,
+                       show_cluster_rows=TRUE,
                        cluster_num=NULL,
                        tree_height_rows=0.1,
                        tree_height_cols=0.1,
@@ -79,7 +177,7 @@ ggheatmap <- function(data,
                        ){
   #step1.scale data
                          if(scale=="column"){
-                         scale_df <- t(scale(data))
+                         scale_df <- scale(data)
                        }else if(scale=="row"){
                          scale_df <- t(scale(t(data)))
                        }else{
@@ -90,17 +188,17 @@ ggheatmap <- function(data,
                            rownames_to_column(var = "gene")%>%
                            tidyr::gather(key = "cluster",value = "expression",-gene)
                          if(cluster_rows){
-                           row_clust <- hclust(dist(scale_df %>% as.matrix(),method = dist_method),method = hclust_method)
+                           row_clust <- hclust(dist(as.matrix(scale_df),method = dist_method),method = hclust_method)
                            roworder <- row_clust$labels[row_clust$order]
 
                            dat$gene <- factor(dat$gene,levels = roworder)
                            if(show_cluster_rows){
                              row_ggtreeplot <- fviz_dend(row_clust, k = cluster_num[1],
-                                                         cex = 0.5,
                                                          k_colors = tree_color_rows,
                                                          color_labels_by_k = TRUE,
-                                                         label_cols = NA,
-                                                         horiz = T,
+                                                         show_labels = FALSE,
+                                                         labels_track_height = 0,
+                                                         horiz = TRUE,
                                                          ggtheme = theme_classic())+
                                theme(title = element_blank(),
                                      axis.text.x = element_blank(),
@@ -124,15 +222,15 @@ ggheatmap <- function(data,
 
 
                         if(cluster_cols){
-                           cols_clust <-  hclust(dist(scale_df %>% as.matrix()%>% t(),method = dist_method),method = hclust_method)
+                           cols_clust <-  hclust(dist(t(as.matrix(scale_df)),method = dist_method),method = hclust_method)
                            colorder <- cols_clust$labels[cols_clust$order]
                            dat$cluster <- factor(dat$cluster,levels = colorder)
                            if(show_cluster_cols){
                             col_ggtreeplot <- fviz_dend(cols_clust, k = cluster_num[2],
-                                                        cex = 0.5,
                                                         k_colors = tree_color_cols,
                                                         color_labels_by_k = TRUE,
-                                                        label_cols = NA,
+                                                        show_labels = FALSE,
+                                                        labels_track_height = 0,
                                                         ggtheme = theme_classic())+
                               theme(title = element_blank(),
                                     axis.text.y = element_blank(),
@@ -165,8 +263,8 @@ ggheatmap <- function(data,
                           p <- ggplot(dat, aes(cluster, gene, fill= expression)) +
                               geom_tile() +
                               scale_fill_gradientn(colours = color)+
-                              scale_y_discrete(position = "right",breaks=text_rows)+
-                              scale_x_discrete(breaks=text_cols)+
+                              scale_y_discrete(position = text_position_rows,breaks=text_rows)+
+                              scale_x_discrete(position = text_position_cols,breaks=text_cols)+
                               theme(axis.title.x = element_blank(),                 #x坐标标题为空
                                     axis.title.y = element_blank(),                 #y坐标标题为空
                                     axis.ticks = element_blank(),
---
+++ R/ggheatmap_test.R
@@ -0,0 +1,12 @@
+#' @title Sample data for ggheatmap
+#' @description This data is simulated data, not real data,
+#' including 12 samples, 50 simulated gene expression data, and 3 annotation data.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ggheatmap_test
+#'
+#'
+#'
+#'
+NULL
