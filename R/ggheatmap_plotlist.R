9309c40156550c464dd39737e293f88aadbbe01f
XiaoLuo-boy <65945397+XiaoLuo-boy@users.noreply.github.com>
2023-03-16 16:43:51 +0800

    Update ggheatmap_plotlist.R

--- R/ggheatmap_plotlist.R
+++ R/ggheatmap_plotlist.R
@@ -9,6 +9,6 @@
 #' @export
 
 ggheatmap_plotlist <- function(ggheatmap){
-  do.call('wrap_plots',ggheatmap[[1]])+
-    plot_annotation(tag_levels = list(paste("plotlist",1:length(ggheatmap[[1]]))))
+  do.call('wrap_plots',ggheatmap$plotlist)+
+    plot_annotation(tag_levels = list(paste("plotlist",1:length(ggheatmap$plotlist))))
   }
